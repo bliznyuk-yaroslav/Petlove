6ab38d87a2baaa099623162e550db0e3017bf69f
Yaroslav Bliznyuk <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2025-05-05 22:17:23 +0300

    365

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import injectHTML from "vite-plugin-html";
-import FullReload from "vite-plugin-full-reload";
-import ViteImageOptimizer from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   plugins: [react()],
